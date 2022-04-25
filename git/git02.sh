f514a973e94844ce558cfd8477103567049bde0e
zdshaoteach <zdshao.teach@gmail.com>
2022-04-25 23:30:02 +0200

    edit

--- git/git02.sh
+++ git/git02.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git config --global user.name "Zongru (Doris) Shao"
-git config --global user.email "drssth@gmail.com"
+git config --global user.email "zdshao.teach@gmail.com"
 git config --global core.autocrlf input
 git config --global init.defaultBranch main
 git config --list
--- git/git07.sh
+++ git/git07.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-#cd ~
-#mkdir workshop
-#cd workshop
-#mkdir planets
-#cd planets
+cd ~
+mkdir workshop
+cd workshop
+mkdir planets
+cd planets
 git init
-ls
-ls -a
+echo "Cold and dry, but everything is my favorite color" > mars.txt
+git status
+rm ~/.ssh/*
+ssh-keygen -t ed25519 -C "zdshao.teach@gmail.com"
 git checkout -b main
 git status
